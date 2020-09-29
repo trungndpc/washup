efb268ce8359a22b9815bc28b5a798324b047740
trungnd2 <trungnd2@vng.com.vn>
2020-09-29 22:12:59 +0700

    updat

--- WashupClient/app/config/production.js
+++ WashupClient/app/config/production.js
@@ -2,6 +2,6 @@ export default {
   __DEV__: true,
   'process.env': {
     NODE_ENV: JSON.stringify('production'),
-    DOMAIN: JSON.stringify('http://wash-up.vn')
+    DOMAIN: JSON.stringify('https://wash-up.vn')
   }
 }
